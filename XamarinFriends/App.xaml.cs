6e7fb0a8fe3e04d8e8951d47ce34e421dd668e6f
veta05 <jelizaveta05@gmail.com>
2023-04-17 14:27:27 +0300

    Добавьте файлы проекта.

---
+++ XamarinFriends/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using XamarinFriends.Views;
+
+namespace XamarinFriends
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new NavigationPage(new FriendsListPage());
+        }
+    }
+}
